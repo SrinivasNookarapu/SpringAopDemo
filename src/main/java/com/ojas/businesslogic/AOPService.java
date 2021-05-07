ed6e6b1d71f42cee7a431d243589ba90ecd17040
SrinivasNookarapu <nookarapusrinivas1@gmail.com>
2021-05-07 09:36:33 +0530

    second commit

---
+++ src/main/java/com/ojas/businesslogic/AOPService.java
@@ -0,0 +1,49 @@
+package com.ojas.businesslogic;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AOPService {
+	
+	@Pointcut("execution(public void com.ojas.businesslogic.BusinessLogic.saveEmployee())")
+	public void p1() {
+		
+	}
+	
+	@Before("p1()")
+	public void tX() {
+		System.out.println("Begin Tx");
+	}
+	
+	@After("p1()")
+	public void commit() {
+		System.out.println("TX commited");
+	}
+	
+	@AfterReturning("p1()")
+	public void commitAr() {
+		System.out.println("@AfterReturning demo");
+	}
+	
+//	@AfterThrowing(value = "p1()", throwing = "th")
+//	public void commitTh(Throwable th) {
+//		System.out.println("Afetr Throwninbg Demo" +th.getMessage());
+//	}
+	
+//	@Around("p1()")
+//	public void aroundDemo() {
+//		System.out.println("Around Demo");
+//	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/ojas/businesslogic/BusinessLogic.java
@@ -0,0 +1,14 @@
+package com.ojas.businesslogic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BusinessLogic {
+	
+	public void saveEmployee() {
+		System.out.println("Employee Save");
+		int num = 10 / 0;
+	}
+	
+
+}
